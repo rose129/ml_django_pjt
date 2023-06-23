4a04f47415e99c20af4bed17dee931f8a25733ed
jihye <mkos__@nate.com>
2023-06-23 11:58:55 +0900

    navbar_footer 분리 추가

---
+++ mydb__env.py
@@ -0,0 +1,6 @@
+host = "localhost"
+user = "root"
+password = "gks2830bnm"
+db = "review_db"
+port = 3306
+charset = "utf8mb4"
--- recruit_pjt/settings.py
+++ recruit_pjt/settings.py
@@ -83,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'recruit_pjt.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -93,7 +92,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -118,8 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 
-
-
 LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
@@ -127,7 +123,6 @@ TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
