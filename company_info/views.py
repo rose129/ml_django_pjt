b16d77f071daa8d7aff89b9b11602b1d15885c1a
rualee <rualee1228@gmail.com>
2023-06-26 16:55:45 +0900

    스타트업랭킹 DB 업데이트

--- company_info/views.py
+++ company_info/views.py
@@ -1,3 +1,4 @@
+import csv
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,13 @@ from django.shortcuts import render
 
 def Companyinfo(request):
     return render( request, 'company_info/company_info.html')
+
+
+# views.py
+
+def csv_to_html(request):
+    with open('파일명.csv', 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+    
+    return render(request, 'table.html', {'data': data})   
